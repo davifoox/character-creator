87cde6cd3df06310b0846a7d88f475389dac16c0
davi-fox-afterverse <136855298+davi-fox-afterverse@users.noreply.github.com>
2023-07-11 16:28:15 -0300

    add - fall anim

--- Character.gd
+++ Character.gd
@@ -9,3 +9,5 @@ func _process(delta):
 		animation_player.play("Run")
 	elif Input.is_action_just_pressed("3"):
 		animation_player.play("Jump")
+	elif Input.is_action_just_pressed("4"):
+		animation_player.play("Fall")
