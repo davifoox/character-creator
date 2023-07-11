2b303ff264f63157c9cd973a46f43747d879076b
davi-fox-afterverse <136855298+davi-fox-afterverse@users.noreply.github.com>
2023-07-11 11:22:47 -0300

    add - number keys changing anims

--- Character.gd
+++ Character.gd
@@ -1,3 +1,9 @@
 extends Node2D
 
+onready var animation_player = $AnimationPlayer
 
+func _process(delta):
+	if Input.is_action_just_pressed("1"):
+		animation_player.play("Idle")
+	elif Input.is_action_just_pressed("2"):
+		animation_player.play("Run")
